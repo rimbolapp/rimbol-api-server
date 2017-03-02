0ab3ea1212753da1d8f9a4ffe841dc7a315a3694
박현재/NE <hyeonjae.park@nhnent.com>
2017-03-02 23:02:58 +0900

    Add simple controller

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('lodash');
 
-/* GET users listing. */
+var sampleUsers = [
+  {
+    id: 101,
+    name: 'kun'
+  },
+  {
+    id: 102,
+    name: 'now'
+  }
+];
+var inc = 102;
+
+///* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.status(200).json(sampleUsers);
+});
+
+///* GET user by id. */
+router.get('/:id', function(req, res, next) {
+  var id = parseInt(req.params.id);
+  var user = _.findLast(sampleUsers, {id: id});
+  res.status(200).json(user);
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body);
+  inc++;
+  var item = {
+    id: inc,
+    name: req.body.name
+  };
+  sampleUsers.push(item);
+  res.status(200).json(item)
 });
 
 module.exports = router;
